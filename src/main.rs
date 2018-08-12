49227af4a03ab7fa5e9db208c2f855ced80dc4d6
Jason Figueroa <jason.figueroa2@gmail.com>
2018-08-11 20:28:40 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is: {}", secret_number);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        // minimal example of error handling
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+
+            // parameter _ is a catchall value
+            Err(_) => {
+                println!("Input is invalid.");
+                continue;
+            }
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too low!"),
+            Ordering::Greater => println!("Too high!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
